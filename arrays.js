a54f7cd6fb2d5c5f2a2ad1ebbc5a251c9db9c351
Chana Kupetz <chanakupetz@gmail.com>
2020-02-24 03:36:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,21 +9,11 @@ function destructivelyAddElementToBeginningOfArray(array,element){
   return array;
 }
 
+function addElementToEndOfArray(array,element){
+  return [...array,element];
+}
 
-
-
-
-/* describe('addElementToBeginningOfArray(array, element)', () => {
-  it('adds an element to the beginning of an array', () => {
-    expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-  })
-
-  it('does not alter the original array', () => {
-    const array = [1]
-
-    addElementToBeginningOfArray(array, 'foo')
-
-    expect(array).to.eql([1])
-  })
-
-TODO: Define two more functions, addElementToEndOfArray and destructivelyAddElementToEndOfArray. These functions also take two arguments, an array and an element to add to the end of the array. addElementToEndOfArray should not alter the original array; destructivelyAddElementToEndOfArray should alter the original array. */
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array;
+  }
