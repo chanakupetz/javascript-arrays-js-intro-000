9022fa896aea9e5da90bfe00014dcd2c3e2bff53
Chana Kupetz <chanakupetz@gmail.com>
2020-02-24 04:19:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array,index){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array;
+    return array.slice(1)
 }
