f034f95165a99e6122dc10dec81ca8f96e1e55f3
Chana Kupetz <chanakupetz@gmail.com>
2020-02-24 03:45:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,9 @@ function destructivelyAddElementToEndOfArray(array,element){
   array.push(element)
   return array;
   }
+
+function accessElementInArray(){
+  
+}
+TODO: Define a function in arrays.js called accessElementInArray. 
+The function should accept an array and an index and return the element at that index.
