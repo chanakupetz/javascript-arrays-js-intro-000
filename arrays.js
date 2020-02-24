4d9ad1197a439fbdc27b87e18fef3cc7b2bddc73
Chana Kupetz <chanakupetz@gmail.com>
2020-02-24 03:25:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,element){
+  return [element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array;
+}
+
+
+/*
 var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
 return ['foo' ...array];
-};
+}; */
 
 
 /*
